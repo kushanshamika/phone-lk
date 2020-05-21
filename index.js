2ea21b66db13abf58c75c51d85001454fe49006a
kushanshamika <shamikakushan@gmail.com>
2020-05-21 19:53:02 +0530

    Add operator codes & area codes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const fs = require('fs');
+
 function phone(number){
+
+    let mobileOperatorCodes = fs.readFileSync("./data/mobileOperatorCodes.json");
+    mobileOperatorCodes = JSON.parse(mobileOperatorCodes);
+
+
+    number = number.match(/\d/g);
+    number = number.join("");
+
+    var formatNumber = null;
+    if (number.substring(0, 2) === "94" && number.length && number.substring(2, 4) in mobileOperatorCodes) {
+        formatNumber = number;
+        console.log("ok");
+    }
     return number;
 }
-
+console.log(phone("+94 77-548-948-5"));
 module.exports.phone = phone;
